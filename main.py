43f6dcb99b33c3a388dfd8f91395ada8bddbeb38
LeonardoCordeiro2000 <leocordeiro09@gmail.com>
2023-11-13 16:16:38 -0300

    Adding a countdown mechanism

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 ﻿from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,9 +14,19 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+   count_down(5 * 60)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
 
+    canvas.itemconfig(timer_text, text= f"{count_min}:{count_sec}")
+    
+    if count > 0:
+     window.after(1000, count_down, count - 1)
+   
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -27,7 +38,7 @@ canvas = Canvas(width=200,height=223, bg=YELLOW, highlightthickness=0)
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, highlightthickness=0, font=(FONT_NAME, 50, "bold"))
 title_label.grid(column=1, row=0)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0)
@@ -39,7 +50,7 @@ check_mark.grid(column=1, row=3)
 tomato_img = PhotoImage(file=r"C:\Users\User_\Downloads\pymodoro\tomato.png")
 
 canvas.create_image(100, 112, image = tomato_img)
-canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 
 canvas.grid(column=1, row=1)
 
